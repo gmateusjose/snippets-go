3d22cb2c484a6590319d30ca7ce081be1e8b43ef
Mateus Silva <gmateusjose6@gmail.com>
2022-03-20 04:47:28 -0300

    adding flags to service init

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,11 +1,24 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 )
 
 func main() {
+	// Define a new command line flag with the name 'addr', a default value of
+	// an port and some short help text explaining what the flag controls. The value
+	// of flag will be stored in the addr variable at runtime.
+	addr := flag.String("addr", ":4000", "HTTP network address")
+
+	// Importantly, we use the flag.Parse() function to parse the command-line args
+	// this reads in the command-line flag value and assigns it to the addr
+	// variable. You need to call this "before" you use the addr variable
+	// otherwise it will always contain the defalut value of ":4000". If any error
+	// was encountered during parsing the application will be terminated.
+	flag.Parse()
+
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet", showSnippet)
@@ -15,13 +28,9 @@ func main() {
 	// directory. Note that the path given to the http.Dir function is relative
 	// to the project directory root.
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
-
-	// Use the mux.Handler() function to register the file server as the handler
-	// all the Url paths that start with "/static/". For matching paths, we
-	// strip the "/static" prefix before the request reaches the file server.
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-	log.Println("Starting server on :4000")
-	err := http.ListenAndServe(":4000", mux)
+	log.Printf("Starting server on %s", *addr)
+	err := http.ListenAndServe(*addr, mux)
 	log.Fatal(err)
 }

a9b0736cd38cfc5a93a2241e4debff1f0c701444
Mateus Silva <gmateusjose6@gmail.com>
2022-03-20 11:51:29 -0300

    refactoring configurations

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,33 +4,33 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
-func main() {
-	// Define a new command line flag with the name 'addr', a default value of
-	// an port and some short help text explaining what the flag controls. The value
-	// of flag will be stored in the addr variable at runtime.
-	addr := flag.String("addr", ":4000", "HTTP network address")
+type application struct {
+	errorLog *log.Logger
+	infoLog  *log.Logger
+}
 
-	// Importantly, we use the flag.Parse() function to parse the command-line args
-	// this reads in the command-line flag value and assigns it to the addr
-	// variable. You need to call this "before" you use the addr variable
-	// otherwise it will always contain the defalut value of ":4000". If any error
-	// was encountered during parsing the application will be terminated.
+func main() {
+	addr := flag.String("addr", ":4000", "HTTP network address")
 	flag.Parse()
 
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/snippet", showSnippet)
-	mux.HandleFunc("/snippet/create", createSnippet)
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
-	// Create a file server which serves files out of the "./ui/static"
-	// directory. Note that the path given to the http.Dir function is relative
-	// to the project directory root.
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+	app := &application{
+		errorLog: errorLog,
+		infoLog:  infoLog,
+	}
 
-	log.Printf("Starting server on %s", *addr)
-	err := http.ListenAndServe(*addr, mux)
-	log.Fatal(err)
+	srv := &http.Server{
+		Addr:     *addr,
+		ErrorLog: errorLog,
+		Handler:  app.routes(),
+	}
+
+	infoLog.Printf("Starting server on %s", *addr)
+	err := srv.ListenAndServe()
+	errorLog.Fatal(err)
 }
